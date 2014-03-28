2555bd3bad9ea8e7ae40a727f59bb546d2aa2717
Karel Zak <kzak@redhat.com>
2014-03-28 10:36:05 +0100

    findfs: add ability to work with PART{UUID,LABEL}= too
    Upstream: http://github.com/karelzak/util-linux/commit/c48508c2faa356c48c26d7d0070a6f20ae4ba9a0
    Addresses: http://bugzilla.redhat.com/show_bug.cgi?id=1335671
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- misc-utils/findfs.c
+++ misc-utils/findfs.c
@@ -19,8 +19,7 @@ static void __attribute__((__noreturn__)) usage(int rc)
 {
 	FILE *out = rc ? stderr : stdout;
 	fputs(USAGE_HEADER, out);
-	fprintf(out, _(" %1$s [options] LABEL=<label>\n"
-		       " %1$s [options] UUID=<uuid>\n"),
+	fprintf(out, _(" %s [options] {LABEL,UUID,PARTUUID,PARTLABEL}=<value>\n"),
 		program_invocation_short_name);
 	fputs(USAGE_OPTIONS, out);
 	fputs(USAGE_HELP, out);
@@ -31,7 +30,7 @@ static void __attribute__((__noreturn__)) usage(int rc)
 
 int main(int argc, char **argv)
 {
-	char	*dev, *tk, *vl;
+	char	*dev;
 
 	setlocale(LC_ALL, "");
 	bindtextdomain(PACKAGE, LOCALEDIR);
@@ -43,23 +42,17 @@ int main(int argc, char **argv)
 		 * with version from e2fsprogs */
 		usage(2);
 
-	if (!strncmp(argv[1], "LABEL=", 6)) {
-		tk = "LABEL";
-		vl = argv[1] + 6;
-	} else if (!strncmp(argv[1], "UUID=", 5)) {
-		tk = "UUID";
-		vl = argv[1] + 5;
-	} else if (strcmp(argv[1], "-V") == 0 ||
+	if (strcmp(argv[1], "-V") == 0 ||
 		   strcmp(argv[1], "--version") == 0) {
 		printf(UTIL_LINUX_VERSION);
 		return EXIT_SUCCESS;
 	} else if (strcmp(argv[1], "-h") == 0 ||
 		   strcmp(argv[1], "--help") == 0) {
 		usage(EXIT_SUCCESS);
-	} else
+	} else if (argv[1][0] == '-')
 		usage(2);
 
-	dev = blkid_evaluate_tag(tk, vl, NULL);
+	dev = blkid_evaluate_tag(argv[1], NULL, NULL);
 	if (!dev)
 		errx(EXIT_FAILURE, _("unable to resolve '%s'"),	argv[1]);
 
