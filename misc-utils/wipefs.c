50995fb7df319d489bc7bb3ccf45e89898c82db5
Karel Zak <kzak@redhat.com>
2016-06-20 11:16:51 +0200

    wipefs: call BLKRRPART when erase partition table
    wipefs does not clean gpt header fully, PMBR remains (v2.25 backport)
    
    Addresses: http://bugzilla.redhat.com/show_bug.cgi?id=1054186

--- misc-utils/wipefs.c
+++ misc-utils/wipefs.c
@@ -40,21 +40,24 @@
 #include "c.h"
 #include "closestream.h"
 #include "optutils.h"
+#include "blkdev.h"
 
 struct wipe_desc {
 	loff_t		offset;		/* magic string offset */
 	size_t		len;		/* length of magic string */
 	unsigned char	*magic;		/* magic string */
 
-	int		zap;		/* zap this offset? */
 	char		*usage;		/* raid, filesystem, ... */
 	char		*type;		/* FS type */
 	char		*label;		/* FS label */
 	char		*uuid;		/* FS uuid */
 
-	int		on_disk;
-
 	struct wipe_desc	*next;
+
+	unsigned int	zap : 1,
+			on_disk : 1,
+			is_parttable : 1;
+
 };
 
 enum {
@@ -72,7 +75,7 @@ print_pretty(struct wipe_desc *wp, int line)
 		printf("----------------------------------------------------------------\n");
 	}
 
-	printf("0x%-17jx  %s   [%s]", wp->offset, wp->type, wp->usage);
+	printf("0x%-17jx  %s   [%s]", wp->offset, wp->type, _(wp->usage));
 
 	if (wp->label && *wp->label)
 		printf("\n%27s %s", "LABEL:", wp->label);
@@ -141,7 +144,7 @@ add_offset(struct wipe_desc *wp0, loff_t offset, int zap)
 	wp = xcalloc(1, sizeof(struct wipe_desc));
 	wp->offset = offset;
 	wp->next = wp0;
-	wp->zap = zap;
+	wp->zap = zap ? 1 : 0;
 	return wp;
 }
 
@@ -164,7 +167,7 @@ get_desc_for_probe(struct wipe_desc *wp, blkid_probe pr)
 	const char *off, *type, *mag, *p, *usage = NULL;
 	size_t len;
 	loff_t offset;
-	int rc;
+	int rc, ispt = 0;
 
 	/* superblocks */
 	if (blkid_probe_lookup_value(pr, "TYPE", &type, NULL) == 0) {
@@ -181,7 +184,8 @@ get_desc_for_probe(struct wipe_desc *wp, blkid_probe pr)
 			rc = blkid_probe_lookup_value(pr, "PTMAGIC", &mag, &len);
 		if (rc)
 			return wp;
-		usage = "partition table";
+		usage = N_("partition table");
+		ispt = 1;
 	} else
 		return wp;
 
@@ -199,6 +203,7 @@ get_desc_for_probe(struct wipe_desc *wp, blkid_probe pr)
 
 	wp->type = xstrdup(type);
 	wp->on_disk = 1;
+	wp->is_parttable = ispt ? 1 : 0;
 
 	wp->magic = xmalloc(len);
 	memcpy(wp->magic, mag, len);
@@ -309,10 +314,25 @@ static void do_wipe_real(blkid_probe pr, const char *devname, struct wipe_desc *
 	putchar('\n');
 }
 
+
+static void rereadpt(int fd, const char *devname)
+{
+#ifdef BLKRRPART
+	struct stat st;
+
+	if (fstat(fd, &st) || !S_ISBLK(st.st_mode))
+		return;
+
+	errno = 0;
+	ioctl(fd, BLKRRPART);
+	printf(_("%s: calling ioclt to re-read partition table: %m\n"), devname);
+#endif
+}
+
 static struct wipe_desc *
 do_wipe(struct wipe_desc *wp, const char *devname, int noact, int all, int quiet, int force)
 {
-	int flags;
+	int flags, reread = 0;
 	blkid_probe pr;
 	struct wipe_desc *w, *wp0;
 	int zap = all ? 1 : wp->zap;
@@ -345,8 +365,11 @@ do_wipe(struct wipe_desc *wp, const char *devname, int noact, int all, int quiet
 		if (!wp->on_disk)
 			continue;
 
-		if (zap)
+		if (zap) {
 			do_wipe_real(pr, devname, wp, noact, quiet);
+			if (wp->is_parttable)
+				reread = 1;
+		}
 	}
 
 	for (w = wp0; w != NULL; w = w->next) {
@@ -355,6 +378,10 @@ do_wipe(struct wipe_desc *wp, const char *devname, int noact, int all, int quiet
 	}
 
 	fsync(blkid_probe_get_fd(pr));
+
+	if (reread)
+		rereadpt(blkid_probe_get_fd(pr), devname);
+
 	close(blkid_probe_get_fd(pr));
 	blkid_free_probe(pr);
 	free_wipe(wp0);
