f076c30040be84871e734efdb23a1e06677bf44d
Karel Zak <kzak@redhat.com>
2016-10-27 11:31:25 +0200

    util: linux ng 2.17 mount mtab
    # CVE-2011-1675 - mount fails to anticipate RLIMIT_FSIZE [#738789]
    
    Addresses: http://bugzilla.redhat.com/show_bug.cgi?id=

--- mount/mount.c
+++ mount/mount.c
@@ -1244,6 +1244,7 @@ update_mtab_entry(const char *spec, const char *node, const char *type,
 			printf(_("mount: no %s found - creating it..\n"),
 			       _PATH_MOUNTED);
 		create_mtab ();
+
 	}
 
 	if (!nomtab && mtab_is_writable()) {
@@ -1251,25 +1252,8 @@ update_mtab_entry(const char *spec, const char *node, const char *type,
 			update_mtab (mnt.mnt_dir, &mnt);
 		else if (flags & MS_MOVE)
 			update_mtab(mnt.mnt_fsname, &mnt);
-		else {
-			mntFILE *mfp;
-
-			lock_mtab();
-			mfp = my_setmntent(_PATH_MOUNTED, "a+");
-			if (mfp == NULL || mfp->mntent_fp == NULL) {
-				int errsv = errno;
-				error(_("mount: can't open %s: %s"), _PATH_MOUNTED,
-				      strerror (errsv));
-			} else {
-				if ((my_addmntent (mfp, &mnt)) == 1) {
-					int errsv = errno;
-					error(_("mount: error writing %s: %s"),
-					      _PATH_MOUNTED, strerror (errsv));
-				}
-			}
-			my_endmntent(mfp);
-			unlock_mtab();
-		}
+		else
+			update_mtab(NULL, &mnt);
 	}
 	my_free(mnt.mnt_fsname);
 	my_free(mnt.mnt_dir);
